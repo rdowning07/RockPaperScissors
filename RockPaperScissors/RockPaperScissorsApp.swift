af8e5a273751e3611788bb72a6c5987eb709ec7b
Rob Downing <redowning@gmail.com>
2025-10-26 18:20:52 -0700

    Initial commit: RockPaperScissors project scaffold

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Rob Downing on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
